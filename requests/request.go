0b726868633dcfc636b831c4a8868d77da9644d3
Nermin-Mujagic <nermin.mujagic90@gmail.com>
2025-07-17 12:02:52 +0200

    test CreateRequest

--- requests/request.go
+++ requests/request.go
@@ -16,33 +16,53 @@ const (
 type TransferRequests []TransferRequest
 
 type TransferRequest struct {
-	StudentID     string
-	PreferredDorm []string // "FDV, Poljane, I,II,III..."
-	RoomType      string   // "any, full, single, couple"
-	WithPartner   bool
-	PartnerID     *string
-	DateSubmitted time.Time
-	Status        statusRequest // "pending (0)", "approved (1)", "declined (2)"
+	StudentID      string
+	PreferredDorms []string // "FDV, Poljane, I,II,III..."
+	RoomType       *string  // "any, full, single, couple"
+	WithPartner    bool
+	PartnerID      *string
+	DateSubmitted  time.Time
+	Status         statusRequest // "pending (0)", "approved (1)", "declined (2)"
 }
 
-func CreateTransferRequest(studentID string, preferredDorm []string, roomType string, partnerID *string) (*TransferRequest, error) {
+func validateDormList(preferredDormList []string) ([]string, error) {
+	if len(preferredDormList) == 0 {
+		return []string{"any"}, nil
+	}
+
+	for _, dorm := range preferredDormList {
+		if _, ok := dormList[dorm]; !ok {
+			return nil, fmt.Errorf("Dorm %s is not a valid dorm", dorm)
+		}
+	}
+
+	return preferredDormList, nil
+
+}
+
+func CreateTransferRequest(studentID string, preferredDormList []string, roomType *string, partnerID *string) (*TransferRequest, error) {
 	withPartner := false
 	if partnerID != nil {
 		withPartner = true
 	}
 
 	if studentID == "" {
-		return nil, fmt.Errorf("Student ID is invalid")
+		return nil, fmt.Errorf("Student ID %q is invalid", studentID)
+	}
+
+	dormList, err := validateDormList(preferredDormList)
+	if err != nil {
+		return nil, err
 	}
 
 	newRequest := TransferRequest{
-		StudentID:     studentID,
-		PreferredDorm: preferredDorm,
-		RoomType:      roomType,
-		WithPartner:   withPartner,
-		PartnerID:     partnerID,
-		DateSubmitted: time.Now(),
-		Status:        Pending,
+		StudentID:      studentID,
+		PreferredDorms: dormList,
+		RoomType:       roomType,
+		WithPartner:    withPartner,
+		PartnerID:      partnerID,
+		DateSubmitted:  time.Now(),
+		Status:         Pending,
 	}
 
 	return &newRequest, nil
@@ -55,8 +75,8 @@ func PrintRequests(transferRequests TransferRequests) {
 		if request.Status == Pending {
 			fmt.Printf("Št. prošnje: %d\n", i+1)
 			fmt.Printf("Vlagatelj: %s\n", request.StudentID)
-			fmt.Printf("Izbrani dom(ovi): %v\n", request.PreferredDorm)
-			fmt.Printf("vrsta sobe: %s\n", request.RoomType)
+			fmt.Printf("Izbrani dom(ovi): %v\n", request.PreferredDorms)
+			fmt.Printf("vrsta sobe: %v\n", request.RoomType)
 			if request.WithPartner {
 				fmt.Printf("Z osebo: %s\n", *request.PartnerID)
 			}
--- requests/request_test.go
+++ requests/request_test.go
@@ -1,7 +1,52 @@
 package request
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+const std1 = "ipriimek"
 
 func TestCreateRequest(t *testing.T) {
+	t.Run("test empty student id", func(t *testing.T) {
+		_, err := CreateTransferRequest("", []string{}, nil, nil)
+		AssertNoError(t, err)
+	})
+	t.Run("test valid dorm", func(t *testing.T) {
+		req, err := CreateTransferRequest(std1, []string{"dom_1"}, nil, nil)
+		AssertNoError(t, err)
+		AssertDeepEqual(t, req.PreferredDorms, []string{"dom_1"})
+	})
 
+	t.Run("test invalid dorm", func(t *testing.T) {
+		_, err := CreateTransferRequest(std1, []string{"dom_schmit"}, nil, nil)
+		AssertError(t, err)
+	})
+
+}
+
+func AssertDeepEqual(t testing.TB, got, want []string) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("expected %+v, got %+v", got, want)
+	}
+}
+
+func AssertEqual(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func AssertError(t testing.TB, got error) {
+	if got == nil {
+		t.Errorf("expected error")
+	}
+}
+
+func AssertNoError(t testing.TB, got error) {
+	if got != nil {
+		t.Errorf("expected no error, got %v", got)
+	}
 }
